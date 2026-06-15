4e2362ba60765c1591f7ccad1c7c317528516f60
arjun-saji-p <arjun2020achu@gmail.com>
2026-06-15 13:28:15 +0530

    Update and rename attendance.py to main.py

---
+++ main.py
@@ -0,0 +1,73 @@
+def menu():
+    print("\n===== STUDENT ATTENDANCE SYSTEM =====")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Mark Attendance")
+    print("4. View Attendance")
+    print("5. Exit")
+
+def add_student():
+    name = input("Enter student name: ")
+
+    with open("students.txt", "a") as file:
+        file.write(name + "\n")
+
+    print("Student added successfully!")
+
+def view_students():
+    try:
+        with open("students.txt", "r") as file:
+            students = file.readlines()
+
+        print("\nStudent List:")
+        for student in students:
+            print(student.strip())
+
+    except FileNotFoundError:
+        print("No students found.")
+
+def mark_attendance():
+    name = input("Enter student name: ")
+    status = input("Present (P) / Absent (A): ")
+
+    with open("attendance.txt", "a") as file:
+        file.write(f"{name},{status}\n")
+
+    print("Attendance marked successfully!")
+
+def view_attendance():
+    try:
+        with open("attendance.txt", "r") as file:
+            records = file.readlines()
+
+        print("\nAttendance Records:")
+
+        for record in records:
+            print(record.strip())
+
+    except FileNotFoundError:
+        print("No attendance records found.")
+
+while True:
+    menu()
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        add_student()
+
+    elif choice == "2":
+        view_students()
+
+    elif choice == "3":
+        mark_attendance()
+
+    elif choice == "4":
+        view_attendance()
+
+    elif choice == "5":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice!")
